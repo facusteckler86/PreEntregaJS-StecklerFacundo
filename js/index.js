7aa5e009e7e365f94937a759f54316473ebf2887
facusteckler86 <facundomsteckler@gail.com>
2023-02-17 17:04:30 -0300

    segundo commit

--- js/index.js
+++ js/index.js
@@ -1,4 +1,34 @@
-let producto;
-let precio;
+/* 
+    Indico al cliente el valor a pagar por las facturas.
+ */
 
-console.log();
+let saludo = "Hola"
+let producto = medialunas;
+const PRECIO = 450;
+
+
+
+let mensaje = prompt(`${saludo} que desea comprar`);
+
+
+
+while (true){
+
+    
+}
+class Productos {
+
+    constructor Producto(nombre, precio, imagen, cantidad) {
+        this.nombre = nombre;
+        this.precio = precio;
+        this.imagen = imagen;
+        
+    
+    }
+
+
+}
+const Producto1 = new Producto (Medialunas, 350, img, 15);
+console.log(Producto1);
+
+
