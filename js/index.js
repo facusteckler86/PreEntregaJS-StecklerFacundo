40b645c2e5d3387dcda3dbfae171d5b103c7c7aa
facusteckler86 <facundomsteckler@gail.com>
2023-02-18 02:04:53 -0300

    tercer commit

--- js/index.js
+++ js/index.js
@@ -1,34 +1,29 @@
 /* 
-    Indico al cliente el valor a pagar por las facturas.
+    El cliente debe elegir entre los dos productos disponibles
  */
 
 let saludo = "Hola"
-let producto = medialunas;
-const PRECIO = 450;
+let producto1 = medialunas;
+let producto2 = pan de campo;
+let producto3 = tortas;
 
 
+let mensaje = parseInt (prompt(`${saludo} Indique por favor que producto desea de nuestra seleccion: `));
 
-let mensaje = prompt(`${saludo} que desea comprar`);
+while (mensaje != "Salir"){
 
-
-
-while (true){
-
+if (producto1 === "Si"){
+    alert (prompt (`Desea comprar medialunas?`))
     
-}
-class Productos {
-
-    constructor Producto(nombre, precio, imagen, cantidad) {
-        this.nombre = nombre;
-        this.precio = precio;
-        this.imagen = imagen;
-        
+} else if(producto2 === "Si"){
+    alert (prompt (`Desea comprar Pan de Campo?`))
     
-    }
-
-
+} else if (producto3 === "Si") {
+    alert (prompt (`Desea comprar unas tortas?`))
+    
+}else if (producto1 === "No" && producto2 === "No" producto3 === "No") {
+    alert (prompt (`Si no desea nada escriba Salir`))
+    
+} 
 }
-const Producto1 = new Producto (Medialunas, 350, img, 15);
-console.log(Producto1);
-
 
